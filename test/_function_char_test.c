aa996b6493a9ef64ad801dc956d61b1e3a100e3a
Annece2935 <anne-cecile.colleter@live.fr>
2025-03-24 18:06:05 +0100

    Mise à jour definitions fonctions, readme, fichier printf Co-authored-by: Robin-1998 <Robin-1998@users.noreply.github.com>

---
+++ test/_function_char_test.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * function_char - print a char
+ * @arg: arguments in parameter
+ * Return Nothing
+ */
+void function_char(va_list arg)
+{
+    _putchar(va_arg(arg, int));
+}
+
+int main()
+{
+
+}
